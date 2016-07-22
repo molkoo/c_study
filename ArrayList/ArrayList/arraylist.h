e9606828f4804bf9ffaed393cc490878d7a6a8b1
molkoo <xoduf1447@gmail.com>
2016-07-22 17:49:46 +0900

    arraylist master

--- ArrayList/ArrayList/arraylist.h
+++ ArrayList/ArrayList/arraylist.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef _ARRAYLIST_
+#define _ARRAYLIST_
+
+typedef struct ArrayListNodeType
+{
+	int data;
+} ArrayListNode;
+
+typedef struct ArrayListType
+{
+	int maxElementCount;
+	int currentElementCount;
+	ArrayListNode *pElement;
+} ArrayList;
+
+ArrayList* createArrayList(int maxElementCount);
+void deleteArrayList(ArrayList* pList);
+int isArrayListFull(ArrayList* pList);
+int addALElement(ArrayList* pList, int position, ArrayListNode element);
+int removeALElement(ArrayList* pList, int position);
+ArrayListNode* getALElement(ArrayList* pList, int position);
+void displayArrayList(ArrayList* pList);
+void clearArrayList(ArrayList* pList);
+int getArrayListLength(ArrayList* pList);
+
+#endif // !_ARRAYLIST_
+
+#ifndef _COMMON_LIST_DEF_
+#define _COMMON_LIST_DEF_
+
+#define TRUE 1
+#define FALSE 0
+
+#endif // !_COMMON_LIST_DEF_
